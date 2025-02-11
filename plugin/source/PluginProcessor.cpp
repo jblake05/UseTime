82d9f9a7f465848b8424ec8ceec25b7ec78f5b38
Jeff Blake <jtblake@middlebury.edu>
2025-02-10 19:01:54 -0500

    Processor done

--- plugin/source/PluginProcessor.cpp
+++ plugin/source/PluginProcessor.cpp
@@ -71,12 +71,10 @@ static int writeFile() {
 }
 
 void timerCallback() {
-    totalSeconds +=
+    totalSeconds += (double) getTimerInterval()/1000;
     if (++timesCalled == (int) SAMPLE_INTERVAL * hz) {
         writeFile();
     }
-
-    
 }
 AudioPluginAudioProcessor::~AudioPluginAudioProcessor()
 {
@@ -186,89 +184,13 @@ bool AudioPluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layou
   #endif
 }
 
-struct time {
-    int hour;
-    int minute;
-    int second;
-};
-
-struct time secondToTime(double second) {
-	int hr = (int) (second / 3600);
-
-	second = fmod(second, 3600);
-
-	int min = (int) (second / 60);
-
-	second = fmod(second, 60);
-
-	int sec = (int)(second);
-
-	struct time result = {
-		hr,
-		min,
-		sec
-	};
-
-	return result;
-}
-
 void AudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
                                               juce::MidiBuffer& midiMessages)
 {
+    juce::ignoreUnused(buffer);
     juce::ignoreUnused (midiMessages);
 
     juce::ScopedNoDenormals noDenormals;
-    // auto totalNumInputChannels  = getTotalNumInputChannels();
-    // auto totalNumOutputChannels = getTotalNumOutputChannels();
-
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    // for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-    //     buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    // for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    // {
-    //     auto* channelData = buffer.getWritePointer (channel);
-    //     juce::ignoreUnused (channelData);
-    //     // ..do something to the data...
-    // }
-    elapsedSamples += buffer.getNumSamples();
-
-    srate = getSampleRate();
-
-    if (elapsedSamples > srate / SAMPLE_FACTOR) {
-        double fracSeconds = elapsedSamples / srate;
-
-        totalSeconds += fracSeconds;
-        secondsElapsed += fracSeconds;
-
-        elapsedSamples -= srate / SAMPLE_FACTOR;
-
-        // edit parameter now
-        struct time timeVal = secondToTime(totalSeconds);
-        if (*hour != timeVal.hour)
-            *hour = timeVal.hour;
-        if (*minute != timeVal.minute)
-            *minute = timeVal.minute;
-        if (*second != timeVal.second)
-            *second = timeVal.second;
-        // apvts
-    }
-
-    if (secondsElapsed > SAVE_INTERVAL) {
-        writeFile();
-        secondsElapsed -= SAVE_INTERVAL;
-    }
 }
 
 //==============================================================================
@@ -292,21 +214,14 @@ void AudioPluginAudioProcessor::getStateInformation (juce::MemoryBlock& destData
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
-    // juce::ignoreUnused (destData);
-    juce::MemoryOutputStream (destData, true).writeInt(*hour);
-    juce::MemoryOutputStream (destData, true).writeInt(*minute);
-    juce::MemoryOutputStream (destData, true).writeInt(*second);
-
+    juce::ignoreUnused (destData);
 }
 
 void AudioPluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
-    // juce::ignoreUnused (data, sizeInBytes);
-    *hour = juce::MemoryInputStream (data, static_cast<size_t> (sizeInBytes), false).readInt();
-    *minute = juce::MemoryInputStream (data, static_cast<size_t> (sizeInBytes), false).readInt();
-    *second = juce::MemoryInputStream (data, static_cast<size_t> (sizeInBytes), false).readInt();
+    juce::ignoreUnused (data, sizeInBytes);
 }
 
 //==============================================================================
